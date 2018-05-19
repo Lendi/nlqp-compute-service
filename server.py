cd246ddff01efed9f143484e1bd2ecd191a97611
Lendi <lendivihari@gmail.com>
2018-05-20 00:32:32 +0530

    added 1 restful endpoint

--- server.py
+++ server.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_restplus import Resource, Api, fields
 app = Flask(__name__)
+api = Api(app)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@api.route('/index')
+class Home(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
