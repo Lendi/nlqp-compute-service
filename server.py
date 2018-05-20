073ae6421d94da01ceacfb7f1ec6beef1328ef9c
Lendi <lendivihari@gmail.com>
2018-05-20 20:30:02 +0530

    deleted merge conflicts

--- server.py
+++ server.py
@@ -8,14 +8,13 @@ class Home(Resource):
     def get(self):
         return {'hello': 'world'}
 
-<<<<<<< HEAD
+
 @api.route('/api/v1/query/<string:query>')
 class QueryResource(Resource):
 	def get(self, query):
 		return{'query':query}
 
 
-=======
->>>>>>> 39b9e750c9c3918fc5bb8743ae17a939cbe71996
+
 if __name__ == "__main__":
     app.run(debug=True)
